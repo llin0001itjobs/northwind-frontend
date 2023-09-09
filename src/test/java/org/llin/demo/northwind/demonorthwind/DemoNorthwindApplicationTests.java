32706a5bc6214e15da0f29a715768e9f88d50df9
llin0001itjobs <42655332+llin0001itjobs@users.noreply.github.com>
2023-09-09 17:29:51 -0400

    Add files via upload

---
+++ src/test/java/org/llin/demo/northwind/demonorthwind/DemoNorthwindApplicationTests.java
@@ -0,0 +1,13 @@
+package org.llin.demo.northwind.demonorthwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNorthwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
